6a2e00c9ee5fca2ca5e947b3c27f0878eaed6760
rayyan umair <reach.rayyan1@gmail.com>
2026-02-08 16:33:03 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    document.getElementById("boot-screen").style.display = "none";
+    document.getElementById("app").classList.remove("hidden");
+  }
+});
 
