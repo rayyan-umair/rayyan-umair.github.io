8dac72a6a1453152d50f5a156017dd699d89bbcb
rayyan umair <reach.rayyan1@gmail.com>
2026-02-20 16:32:48 -0500

    Add runLoader function to manage loading animation

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,23 @@
+export function runLoader() {
+  const loader = document.getElementById("loader");
+  const bar = document.getElementById("loader-bar");
 
+  loader.classList.remove("hidden");
+
+  return new Promise((resolve) => {
+    let progress = 0;
+
+    const interval = setInterval(() => {
+      progress += 5;
+      bar.style.width = progress + "%";
+
+      if (progress >= 100) {
+        clearInterval(interval);
+        setTimeout(() => {
+          loader.classList.add("hidden");
+          resolve();
+        }, 300);
+      }
+    }, 40);
+  });
+}
